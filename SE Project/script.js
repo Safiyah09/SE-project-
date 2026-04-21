be68b15c960379926ed3d79abde036a587375073
ZiaFatima41 <ziafatimasavanur@gmail.com>
2026-04-21 18:12:30 +0530

    script.js

--- SE Project/script.js
+++ SE Project/script.js
@@ -41,6 +41,17 @@ document.addEventListener('click', function(event) {
         dropdownMenu.classList.remove('show');
     }
 });
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
+});
 
 // Handle dropdown item clicks
 const dropdownItems = document.querySelectorAll('.dropdown-item');
